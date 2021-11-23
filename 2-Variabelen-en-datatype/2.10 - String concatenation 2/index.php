391c0eadbca6faa153fb730836d975a2923f0924
Pharrell Helstone <2153880@talnet.nl>
2021-11-23 09:05:29 +0100

    opdracht 2.10 af

---
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$item1 = "kip";
+$item2 = "melk";
+$item3 = "noten";
+echo "<ul>$item1 </ul> <ul> $item2</ul>  <ul>$item3</ul>";
+?>
+
+
+
+
+
+</body>
+</html> 
