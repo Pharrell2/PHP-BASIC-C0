5d392608976da1ec78d3b16fa0fc880c56b9c62a
Pharrell Helstone <2153880@talnet.nl>
2021-11-23 01:25:21 +0100

    opdracht 2.6 af

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+
+<?php
+$string = true;
+echo gettype($string) 
+?>
+
+<?php
+$x = 43;
+echo gettype($x)
+?>
+
+<?php
+$y = "hallo apen";
+echo gettype($y)
+?>
+
+<?php
+$P = null;
+echo gettype($P);
+?>
+
+<?php
+$getal = 5.5;
+echo gettype($getal)
+?>
