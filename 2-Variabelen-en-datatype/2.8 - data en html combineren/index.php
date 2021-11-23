12e30e7bb565b847eb1bca2eb4cd3805d98d7dd4
Pharrell Helstone <2153880@talnet.nl>
2021-11-23 01:44:31 +0100

    opdracht 2.8 af

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$voornaam1 = "Arman";
+$voornaam2 = "Eric";
+$voornaam3 = "Recep";
+echo "<li>$voornaam1 </li> <li> $voornaam2</li>  <li>$voornaam3</li>";
+?>
+
+
+
+
+
+</body>
+</html> 
