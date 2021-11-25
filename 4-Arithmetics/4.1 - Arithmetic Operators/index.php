ec726b50722c2869d222d9588bc7188c6c607cd9
Pharrell Helstone <2153880@talnet.nl>
2021-11-25 23:37:15 +0100

    opdracht 4.1 af

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,18 @@
+<?php
+$getal1 = 25;
+$getal2 = 45;
+echo $getal1+$getal2;
+
+$getal1 = 99;
+$getal2 = 71;
+echo $getal1*$getal2;
+
+$getal1 = 81;
+$getal2 = 66;
+echo $getal1-$getal2;
+
+
+$getal1 = 91;
+$getal2 = 22;
+echo $getal1/$getal2;
+?>
