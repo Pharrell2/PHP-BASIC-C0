c17e2069fc0246775f2fa091cc54ccccba241e69
Pharrell Helstone <2153880@talnet.nl>
2021-11-25 09:30:41 +0100

    opdracht 3.5 af

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,9 @@
+<?php
+$Getallen = [676,5345,78657,3,46,21,41];
+asort($Getallen);
+print_r($Getallen);
+
+$Getallen = [3, 3, 5, 21, 6, 41, 4, 46,5345,2, 78657];
+arsort($Getallen);
+print_r($Getallen);
+?>
