7c28551dec966bf57d8366f17baf2f141c7dd0c5
Pharrell Helstone <2153880@talnet.nl>
2021-11-26 13:21:24 +0100

    opdracht 4.2 en 4.3 verbeterd

--- 4-Arithmetics/4.2 -Assignment Operators/index.php
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -1,9 +1,9 @@
 <?php
-$getal1 +=45;
+$getal1 = 45;
 $getal1+=3;
 echo $getal1."<br>";
 
-$getal1 99;
+$getal1 = 99;
 $getal1*=7; 
 echo $getal1;. "<br>";
 
@@ -11,7 +11,7 @@ $getal1 -= 81;
 $getal1-=69;
 echo $getal1."<br>";
 
-$getal1 /= 91;
+$getal1 = 91;
 $getal1/= ;
 echo $getal1."<br>";
 ?>
--- 4-Arithmetics/4.3 - Increment-operators/index.php
+++ 4-Arithmetics/4.3 - Increment-operators/index.php
@@ -1,9 +1,4 @@
 <?Php
 $getal1 =7;
-$getal1++28;
-echo $getal1. "<br>";
-
-$getal1 =19;
-$getal1--5;
-echo $getal1. "<br>";
-?>
+echo $getal1 ++ . "<br>";
+echo $getal1 --;
