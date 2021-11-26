fed37abd2d5dcddb5264bf39a65dbc0da5403a53
Pharrell Helstone <2153880@talnet.nl>
2021-11-26 01:06:52 +0100

    opdracht 4.2 af

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,14 @@
+<?php
+$getal11 +=45;
+echo $getal1;
+echo "<br>"
+$getal1 *=99;
+echo $getal1;
+echo "<br>"
+$getal1 -= 81;
+echo $getal1;
+echo "<br>"
+$getal1 /= 91;
+echo $getal1;
+echo "<br>"
+?>
