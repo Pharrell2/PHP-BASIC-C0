b902ff4dadec5ee6d311193fc1254ef91b955f46
Pharrell Helstone <2153880@talnet.nl>
2021-11-26 13:45:38 +0100

    5.1 af

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,12 @@
+<?php
+$test1 =15;
+$test2 = "broodje bapao";
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
+?>
