55d73f0f2e1781322bc356d843da979411d4a4d2
Pharrell Helstone <2153880@talnet.nl>
2022-01-27 11:28:19 +0100

    5.4 verbeterd

--- 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -1,27 +1,13 @@
 <?php
-$prod_besteld = 12;
-
+$prod_besteld = 25;
 if($prod_besteld < 10) {
     $prijs = 1.50;
-} elseif($prod_besteld < 20) {
-  echo  $prijs = 1.25 . "<br>";
-} else {
-    $prijs = 1.00;
-}
+} 
 
-if($prod_besteld < 10) {
-      $prijs = 1.25;
-  } elseif($prod_besteld < 20) {
-    echo  $prijs = 1.50 . "<br>";
-  } else {
-      $prijs = 1.00;
-  }
-
-  if($prod_besteld < 10) {
-      $prijs = 1.50;
-  } elseif($prod_besteld < 20) {
-    echo  $prijs = 1.00 . "<br>";
-  } else {
-      $prijs = 1.25;
-  }
+elseif($prod_besteld  <20) {
+  echo  $prijs = 1.25;
+} 
+else {
+    echo  $prijs = 1.00;
+  }  
 ?>
