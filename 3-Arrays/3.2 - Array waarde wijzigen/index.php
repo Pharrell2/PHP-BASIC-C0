b5156f9a06d112db884cf15e2388a32a503a8edf
Pharrell Helstone <2153880@talnet.nl>
2022-01-27 11:17:22 +0100

    3.2 verbeterd

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -1,10 +1,10 @@
 <?php
-$myDogNames= [ "Bikki" , "George" , "Rimboestok" ];
+$myDogNames = [ "Bikki" , "George" , "Rimboestok" ];
 echo $myDogNames[0] . "<br>";
 echo $myDogNames[1] .  "<br>";
-echo $myDogNames[2] .  "<br>";
+echo $myDogNames[2] .  "<br><br>";
 
-$myDogNames[0] = 'Gtpipelstick the firstyoda';
+$myDogNames[0] = 'Gt_pipelstick_ the_ first_yoda';
 echo $myDogNames [0]  .  "<br>";
 echo $myDogNames [1]  .  "<br>";
 echo $myDogNames [2]  .  "<br>";
